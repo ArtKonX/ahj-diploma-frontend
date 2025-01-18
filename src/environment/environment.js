40b79059be026b41633eb96270922c5f8f86feb7
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-01-18 23:47:40 +0300

    Fixed error page server

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,4 +1,4 @@
 const API_ROOT =
-  process.env.API_ROOT || "https://ahj-diploma-backend-kgk7.onrender.com/";
+  process.env.API_ROOT || "https://ahj-diploma-backend-kgk7.onrender.com";
 
 export default API_ROOT;
